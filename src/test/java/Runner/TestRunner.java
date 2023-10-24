b18091995d77c97a64c6ae40b116e789874bcd5c
Farzana Afzali <afzalifarzana297@gmail.com>
2023-10-23 22:26:09 -0400

    Knights_Karate_Test

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -4,6 +4,7 @@ import com.intuit.karate.junit5.Karate;
 public class TestRunner {
     @Karate.Test
     Karate run(){
-        return Karate.run("src/test/java/lab_Session");
+        return Karate.run("src/test/java/lab_Session")
+                .outputCucumberJson(true);
     }
 }
